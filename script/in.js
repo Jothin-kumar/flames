4968134fc74bf3bd0d24072230b6b53e28ef6ae6
Jothin Kumar <contact@jothin.tech>
2024-03-24 14:28:06 +0530

    not compatible with onscreen keyboards

--- script/in.js
+++ script/in.js
@@ -1,61 +1,11 @@
-function loadedIn() {
-    const ins = document.querySelectorAll('input')
-    for (let i = 0; i < ins.length; i++) {
-        const inEl = ins[i]
-        function handler(elem) {
-            const val = elem.value
-            const l = val.length
-            if (l === 1) {
-                elem.value = ""
-            }
-            elem.setSelectionRange(l - 1, l);
-        }
-        inEl.addEventListener('keydown', function(e) {
-            const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            e.preventDefault()
-            if (inEl.value.length >= 21 && e.key !== "Backspace") {
-                return
-            }
-            if (alphabets.includes(e.key) || e.key === " ") {
-                inEl.value = inEl.value.slice(0, -1) + e.key + "|"
-            }
-            if (e.key === "Backspace") {
-                inEl.value = inEl.value.slice(0, -2) + "|"
-            }
-            handler(inEl)
-        })
-        inEl.addEventListener('focusout', function() {
-            if (inEl.value.endsWith("|")) {
-                inEl.value = inEl.value.slice(0, -1)
-            }
-        })
-        inEl.addEventListener('focus', function() {
-            if (inEl.value && !inEl.value.endsWith("|")) {
-                inEl.value += "|"
-                const l = elem.value.length
-                inEl.setSelectionRange(l - 1, l);
-            }
-        })
-    }
-
-    window.flamesButtonClicked = () => {
-        if (document.getElementById('your-name').value && document.getElementById('crush-name').value) {
-            calc()
-            for (let i = 0; i < ins.length; i++) {
-                ins[i].disabled = true
-            }
-            const btn = document.getElementById("flames-btn")
-            btn.disabled = true
-            btn.classList.add('disabled-flames-btn')
-        }
-    }
-    window.resetIn = () => {
+window.flamesButtonClicked = () => {
+    if (document.getElementById('your-name').value && document.getElementById('crush-name').value) {
+        calc()
         for (let i = 0; i < ins.length; i++) {
-            ins[i].value = ""
-            ins[i].disabled = false
+            ins[i].disabled = true
         }
         const btn = document.getElementById("flames-btn")
-        btn.disabled = false
-        btn.classList.remove('disabled-flames-btn')
+        btn.disabled = true
+        btn.classList.add('disabled-flames-btn')
     }
 }
